48974204e898a90c2db55d65f53404eaec33e6e7
Leon <leon.krug@purpur.ventures>
2025-03-30 09:49:55 +0000

    changed launch file for spot

--- launch/grid_trajectory.launch.py
+++ launch/grid_trajectory.launch.py
@@ -16,17 +16,17 @@ def generate_launch_description():
             output='screen',
             parameters=[
                 {'cloud_in_topic': LaunchConfiguration("topic")},
-                {'position_x': -20.0},  # meters
+                {'position_x': 0.0},  # meters
                 {'position_y': 0.0},  # meters
                 {'verbose1': False},
                 {'verbose2': False},
                 {'cell_size': 0.2},  # meters
                 {'length_x': 40.0},  # meters
-                {'length_y': 60.0},  # meters
-                #{'frame_out': 'os1_sensor'},
+                {'length_y': 40.0},  # meters
+                {'frame_out': 'base_link'},
                 {'mapi_topic_name': 'intensity_grid'},
                 {'maph_topic_name': 'height_grid'},
-                {'search_length': 4.0}, # meters
+                {'search_length': 10.0}, # meters
                 {'search_range_deg': 80.0}, # degrees
                 {'search_resolution_deg': 0.5}, # degrees
                 {'search_start_mid_deg': -180.0}, # degrees
